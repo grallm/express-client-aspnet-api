0a1bef00a65b3e8e3e09deee47005fa8251a1faa
Malo Grall <malo.grall@gmail.com>
2021-04-28 23:40:11 +0100

    Logoug + created booking page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
+const axios = require('axios')
 var express = require('express');
 var router = express.Router();
 
+const apiUrl = 'http://localhost:5000'
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  if (req.session.user) {
+    const bookings = await axios.get(`${apiUrl}/api/SessionBookingsAPI/`)
+    console.log(bookings)
+    return res.render('bookings', { title: 'MaxFitness App - Bookings' });
+  }
+  res.render('index', { title: 'MaxFitness App' });
 });
+
+// Logout user
+router.get('/logout', function(req, res) {
+  req.session.destroy()
+  res.redirect('/')
+})
+
+// Login user
+router.post('/login', async function(req, res) {
+  if (req.body.login) {
+    // Check user's login
+    const response = await axios.post(`${apiUrl}/api/SessionBookingsAPI/CheckLogin?login=${req.body.login}`)
+    if (response) {
+      req.session.user = response.data
+    }
+  }
+  res.redirect('/')
+})
 
 module.exports = router;
